eb911735e6ee9d1b77424fbe3ef8731c78269eb4
Nagib Mahfuz <120543584+nagibgithub@users.noreply.github.com>
2023-07-07 01:42:01 +0600

    check branch

--- index.js
+++ index.js
@@ -27,22 +27,6 @@ const run = async () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         // Send a ping to confirm a successful connection
         client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
